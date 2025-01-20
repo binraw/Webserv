46aae5212db4e1bb20ae38c1e2ae04658547d83e
Florian Berthouly <fberthou@z1r8p1.42lyon.fr>
2025-01-20 09:00:44 +0100

    maj Cluster

--- includes/webserv.hpp
+++ includes/webserv.hpp
@@ -11,18 +11,19 @@
 /* -------------------------------------- */
 /* --- DEFINE GENERIC ERROR MESSAGES  --- */
 /* -------------------------------------- */
-#define ERROR       "Error" << std::endl
+#define ERROR	   "Error" << std::endl
 #define PRINTUSAGE  std::cout   << YELLOW "USAGE :" << std::endl \
-                        << "    -> a path to a file config (.conf)" << std::endl \
-                        << "    -> no argument and default server is launch" \
-                        << RESET << std::endl
+						<< "	-> a path to a file config (.conf)" << std::endl \
+						<< "	-> no argument and default server is launch" \
+						<< RESET << std::endl
 
 /* -------------------------------------- */
-/* ---          DEFINE PATH           --- */
+/* ---		  DEFINE PATH		   --- */
 /* -------------------------------------- */
-#define PATH_DEFAULTSERV    "./config/default.conf"
-#define PATH_DEBUGSERV      "./config/debug.conf"
-#define PATH_MIME           "./config/mime.types"
+#define PATH_DEFAULTSERV	"./config/default.conf"
+#define PATH_DEBUGSERR		"./config/debug.conf"
+#define PATH_ERRPAGE		"./error_pages"
+#define PATH_MIME			"./config/mime.types"
 
 
 #endif
--- sources/server/Cluster.hpp
+++ sources/server/Cluster.hpp
@@ -10,102 +10,134 @@
 #include <string>
 #include <vector>
 
-/*
-    structure des parametres par default de l'ensemble des clusters
-*/
-struct s_clusterDefault
-{
-    std::vector<std::string> protocols_handle_by_webserv;
-    std::vector<int> numbers_of_servers_by_clusters;
-    
-    s_clusterDefault()
-      : protocols_handle_by_webserv(1, "http"),
-        numbers_of_servers_by_clusters(1, 1)
-    {   }
-};
-typedef struct s_clusterDefault t_clusterDefault;
+#include <netdb.h>
 
 class AServer;
 class ClusterException;
 
+/*
+	structure des parametres par default de l'ensemble des clusters
+*/
+struct s_clusterDefault
+{
+	std::vector<std::string>	protocols_handle_by_webserv;
+	std::vector<int>			numbers_of_servers_by_clusters;
+		
+	s_clusterDefault()
+	  : protocols_handle_by_webserv(1, "http"),
+		numbers_of_servers_by_clusters(1, 1)
+	{   }
+};
+
+/*
+	contient les 
+*/
+struct s_protocolData {
+	protoent
+		* protocolInfo;
+	
+	std::vector<std::string>
+		include; // ./error_pages ;
+	
+	std::vector<std::string>
+		default_type; // ;
+	
+	int
+		keepalive_timeout; // 65;
+	
+	int
+		worker_connexion; // 1024
+
+	s_protocolData()
+	  : protocolInfo(getprotobyname("tcp")),
+	  	include( { "include", {PATH_ERRPAGE, PATH_MIME} } ),
+		default_type( { "default_type", { "application/octet-stream" } } ),
+		keepalive_timeout( {} )
+	{   }
+};
+
+
+typedef struct s_clusterDefault t_clusterDefault;
+typedef struct s_protocolData   t_protocolData;
+
 class Cluster
 {
-    public:
-        Cluster(const std::string & file) throw(ClusterException);
-        Cluster(const Cluster &);
-        virtual ~Cluster();
-        
-        Cluster &               operator=(const Cluster &) const;
-        friend std::ostream &   operator<<(std::ostream &, const Cluster &);
-    protected:
+	public:
+		Cluster(const std::string & file) throw(ClusterException);
+		Cluster(const Cluster &);
+		virtual ~Cluster();
+		
+		Cluster &			   operator=(const Cluster &) const;
+		friend std::ostream &   operator<<(std::ostream &, const Cluster &);
+	protected:
 
 
-    private:
-        // MEMBERS //
-        const std::string
-            & _configPath;
-        /*  _configPath
-            copie du path du fichier de config pour eviter de le passer en parametre aux fonctions
-        */
-        t_clusterDefault    _clusterDefault;
-        /*  _clusterDefault
-            structure qui contient tous les parametres du cluster par defaut
-            elle permetra de pouvoir lancer un cluster classique http
-            ou bien plusieur cluster avec des protocoles differents
-        */
-        
-        std::map<std::string, std::vector<std::string>>
-            & _configData;
-        /*  _clusterData
-            key : nom du protocole
-            val : ensemble des tokens de description des serveurs SANS LES PARAMS DU CLUSTER
+	private:
+		// MEMBERS //
+		const std::string
+			& _configPath;
+		/*  _configPath
+			copie du path du fichier de config pour eviter de le passer en parametre aux fonctions
+		*/
+		t_clusterDefault	_clusterDefault;
+		/*  _clusterDefault
+			structure qui contient tous les parametres du cluster par defaut
+			elle permetra de pouvoir lancer un cluster classique http
+			ou bien plusieur cluster avec des protocoles differents
+		*/
+		
+		std::map<t_protocolData, std::vector<std::string>>
+			& _configData;
+		/*  _clusterData
+			key : nom du protocole
+			val : ensemble des tokens de description des serveurs SANS LES PARAMS DU CLUSTER
 
-            cette structure de donnee est le resultat final du parsing des blocs protocole (http / https)
-            elle sera donnee en argument a la fonction d'initialisation des serveurs
-        */
+			cette structure de donnee est le resultat final du parsing des blocs protocole (http / https)
+			elle sera donnee en argument a la fonction d'initialisation des serveurs
+		*/
 
-        std::map<std::string, std::vector<AServer*>>
-            _service_servers;
-        /*  _service_servers
+		std::map<std::string, std::vector<AServer *>>
+			_service_servers;
+		/*  _service_servers
 
-            schema de la structure attendue
-            _service_servers {
-                {"http", {AServer1*, AServer2*, AServer3*}},
-                {"https", {AServer1*, AServer2*, AServer3*}}
-            }
-        */
+			schema de la structure attendue
+			_service_servers {
+				{"http", {AServer1*, AServer2*, AServer3*}},
+				{"https", {AServer1*, AServer2*, AServer3*}}
+			}
+		*/
 
-        // METHODS //
-        std::map<std::string, std::map<std::string, std::string>>
-            & parseFile(void);
-        std::vector<std::string>
-            & getFile(void);
-        std::map<std::string, std::map<std::string, std::string>>
-            & getAllProto(const std::vector<std::string> & allFile);
+		// METHODS //
+		std::map<std::string, std::map<std::string, std::string>>
+			& parseFile(void);
+		std::vector<std::string>
+			& getFile(void);
+		std::map<std::string, std::map<std::string, std::string>>
+			& getAllProto(const std::vector<std::string> & allFile);
 
-        void
-            setCluster(const std::vector<std::string> &);
-        /*
-            infos necessaires avant initialisation des serveurs :
-                - le nb de protocoles differents (HTTP HTTPS IRC etc)
-                - le nb de serveur virtuelles a initialiser par protocoles
+		void
+			setCluster(const std::vector<std::string> &);
+		/*
+			infos necessaires avant initialisation des serveurs :
+				- le nb de protocoles differents (HTTP HTTPS IRC etc)
+				- le nb de serveur virtuelles a initialiser par protocoles
 
-            il me faut une liste de parametres par protocole
-                pour modifier les parametres static du tableau de params de Cluster
-                un tableau de params par protocoles (si plusieurs sont pris en charge, renvoie une erreur si le protocole n'est pas pris en charge (ex HTTPS))
+			il me faut une liste de parametres par protocole
+				pour modifier les parametres static du tableau de params de Cluster
+				un tableau de params par protocoles (si plusieurs sont pris en charge, renvoie une erreur si le protocole n'est pas pris en charge (ex HTTPS))
 
-            
-        */
+			
+		*/
 
-        // EXCEPT CLASS //
-        class ClusterException : std::exception
-        {
-            public:
-                ClusterException() {};
-                void            openfile(const std::string & filepath) const throw();
-                const char *    what() const throw();
+		// EXCEPT CLASS //
+		class ClusterException : std::exception
+		{
+			public:
+				ClusterException() {};
+				void			openfile(const std::string & filepath) const throw();
+				const char *	what() const throw();
 
-        };
+		};
 };
 
 #endif
